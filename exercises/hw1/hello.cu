768076f0a5fa84cf5912727c9aa7dbbb2cb5b27d
rsnm2 <rshaw@neuralmagic.com>
2023-07-06 23:08:32 +0000

    finished 1a - hello worlds

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -2,12 +2,13 @@
 
 __global__ void hello(){
 
-  printf("Hello from block: %u, thread: %u\n", FIXME);
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  printf("Hello from index: %u (block: %u, thread: %u)\n", index, blockIdx.x, threadIdx.x);
 }
 
 int main(){
 
-  hello<<<FIXME>>>();
+  hello<<<2, 2>>>();
   cudaDeviceSynchronize();
 }
 
