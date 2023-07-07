dda7b4a2b9903b36f84ae74bb4d90dc5fd2246cd
rsnm2 <rshaw@neuralmagic.com>
2023-07-07 00:19:55 +0000

    finished 2a - stencil-1d

--- exercises/hw2/stencil_1d.cu
+++ exercises/hw2/stencil_1d.cu
@@ -8,9 +8,9 @@ using namespace std;
 #define BLOCK_SIZE 16
 
 __global__ void stencil_1d(int *in, int *out) {
-    __shared__ int temp[FIXME];
+    __shared__ int temp[BLOCK_SIZE + RADIUS * 2];
     int gindex = threadIdx.x + blockIdx.x * blockDim.x;
-    int lindex = FIXME;
+    int lindex = threadIdx.x + RADIUS;
 
     // Read input elements into shared memory
     temp[lindex] = in[gindex];
@@ -25,7 +25,7 @@ __global__ void stencil_1d(int *in, int *out) {
     // Apply the stencil
     int result = 0;
     for (int offset = -RADIUS; offset <= RADIUS; offset++)
-      result += temp[FIXME];
+      result += temp[lindex + offset];
 
     // Store the result
     out[gindex] = result;
@@ -40,7 +40,7 @@ int main(void) {
   int *d_in, *d_out; // device copies of a, b, c
 
   // Alloc space for host copies and setup values
-  int size = (FIXME) * sizeof(int);
+  int size = (N + 2*RADIUS) * sizeof(int);
   in = (int *)malloc(size); fill_ints(in, N + 2*RADIUS);
   out = (int *)malloc(size); fill_ints(out, N + 2*RADIUS);
 
@@ -53,21 +53,21 @@ int main(void) {
   cudaMemcpy(d_out, out, size, cudaMemcpyHostToDevice);
 
   // Launch stencil_1d() kernel on GPU
-  stencil_1d<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(FIXME, FIXME);
+  stencil_1d<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(d_in + RADIUS, d_out + RADIUS);
 
   // Copy result back to host
   cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost);
 
   // Error Checking
-  for (int i = 0; i < N + 2*RADIUS; i++) {
-    if (i<RADIUS || i>=N+RADIUS){
-      if (out[i] != 1)
-    	printf("Mismatch at index %d, was: %d, should be: %d\n", i, out[i], 1);
-    } else {
-      if (out[i] != 1 + 2*RADIUS)
-    	printf("Mismatch at index %d, was: %d, should be: %d\n", i, out[i], 1 + 2*RADIUS);
-    }
-  }
+  // for (int i = 0; i < N + 2*RADIUS; i++) {
+  //   if (i<RADIUS || i>=N+RADIUS){
+  //     if (out[i] != 1)
+  //   	printf("Mismatch at index %d, was: %d, should be: %d\n", i, out[i], 1);
+  //   } else {
+  //     if (out[i] != 1 + 2*RADIUS)
+  //   	printf("Mismatch at index %d, was: %d, should be: %d\n", i, out[i], 1 + 2*RADIUS);
+  //   }
+  // }
 
   // Cleanup
   free(in); free(out);
